f7dc6b9c01c3b76048016289cab3dbf034282bfa
TOSS <tiago.medice@gft.com>
2020-03-01 17:42:30 +0100

    release

--- src/main/java/com/medici/app/config/AutoConfiguration.java
+++ src/main/java/com/medici/app/config/AutoConfiguration.java
@@ -4,7 +4,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan
+@ComponentScan("com.medici")
 public class AutoConfiguration {
 
 }
