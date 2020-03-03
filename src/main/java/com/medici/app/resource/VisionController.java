2a9734d1a71eeb83aaa3f57dfb811eca7fcc1fc6
TOSS <tiago.medice@gft.com>
2020-03-03 07:56:35 +0100

    ref

--- src/main/java/com/medici/app/resource/VisionController.java
+++ src/main/java/com/medici/app/resource/VisionController.java
@@ -1,19 +1,24 @@
 package com.medici.app.resource;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Arrays;
-import java.util.Collection;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.logging.Logger;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.google.api.gax.core.FixedCredentialsProvider;
+import com.google.auth.Credentials;
+import com.google.auth.oauth2.ServiceAccountCredentials;
 import com.google.cloud.vision.v1.AnnotateImageRequest;
 import com.google.cloud.vision.v1.AnnotateImageResponse;
 import com.google.cloud.vision.v1.BatchAnnotateImagesResponse;
@@ -21,6 +26,7 @@ import com.google.cloud.vision.v1.Feature;
 import com.google.cloud.vision.v1.Feature.Type;
 import com.google.cloud.vision.v1.Image;
 import com.google.cloud.vision.v1.ImageAnnotatorClient;
+import com.google.cloud.vision.v1.ImageAnnotatorSettings;
 import com.google.protobuf.Descriptors.FieldDescriptor;
 import com.medici.app.entity.VisionMessage;
 import com.medici.app.utils.ByteUtils;
@@ -35,11 +41,13 @@ public class VisionController {
 
 	protected Logger logger = Logger.getLogger(VisionController.class.getName());
 
-	@RequestMapping(value = "/faceDetection", method = RequestMethod.POST)
-	public Collection<?> faces(@RequestBody VisionMessage message) {
-		try {
+	@Autowired
+	ResourceLoader resourceLoader;
 
-			ImageAnnotatorClient vision = ImageAnnotatorClient.create();
+	@RequestMapping(value = "/faceDetection", method = RequestMethod.POST)
+	public String faces(@RequestBody VisionMessage message) {
+
+		try (ImageAnnotatorClient vision = ImageAnnotatorClient.create(getImageAnnotatorSettings())) {
 			Image img = ByteUtils.getByteString(message);
 			Feature feat = Feature.newBuilder().setType(Type.FACE_DETECTION).build();
 
@@ -50,56 +58,52 @@ public class VisionController {
 				out.putAll(entity.getAllFields());
 			}));
 
-			return out.values();
+			return responses.toString();
 
 		} catch (IOException e) {
-			return Collections.EMPTY_LIST;
+			return org.apache.commons.lang3.StringUtils.EMPTY;
 		}
 	}
 
 	@RequestMapping(value = "/labelDetection", method = RequestMethod.POST)
-	public Collection<?> labels(@RequestBody VisionMessage message) {
+	public String labels(@RequestBody VisionMessage message) {
 
-		try (ImageAnnotatorClient vision = ImageAnnotatorClient.create()) {
+		try (ImageAnnotatorClient vision = ImageAnnotatorClient.create(getImageAnnotatorSettings())) {
 
 			Image img = ByteUtils.getByteString(message);
 			Feature feat = Feature.newBuilder().setType(Type.LABEL_DETECTION).build();
 
 			List<AnnotateImageResponse> responses = getResponses(vision, feat, img);
 
-			Map<FieldDescriptor, Object> out = new HashMap<>();
-			responses.stream().forEach(r -> r.getLabelAnnotationsList().stream().forEach(entity -> {
-				out.putAll(entity.getAllFields());
-			}));
-
-			return out.values();
+			return responses.toString();
 
 		} catch (IOException e) {
-			return Collections.EMPTY_LIST;
+			return org.apache.commons.lang3.StringUtils.EMPTY;
 		}
 	}
 
 	@RequestMapping(value = "/text", method = RequestMethod.POST)
-	public Collection<?> text(@RequestBody VisionMessage message) {
+	public String text(@RequestBody VisionMessage message) {
 
-		try (ImageAnnotatorClient vision = ImageAnnotatorClient.create()) {
+		try (ImageAnnotatorClient vision = ImageAnnotatorClient.create(getImageAnnotatorSettings())) {
 
 			Image img = ByteUtils.getByteString(message);
 			Feature feat = Feature.newBuilder().setType(Type.TEXT_DETECTION).build();
 
 			List<AnnotateImageResponse> responses = getResponses(vision, feat, img);
-
-			Map<FieldDescriptor, Object> out = new HashMap<>();
-			responses.stream().forEach(r -> r.getTextAnnotationsList().stream().forEach(entity -> {
-				out.putAll(entity.getAllFields());
-			}));
-
-			return out.values();
+			return responses.toString();
 
 		} catch (IOException e) {
-			return Collections.EMPTY_LIST;
+			return org.apache.commons.lang3.StringUtils.EMPTY;
 		}
 
+	}
+
+	private ImageAnnotatorSettings getImageAnnotatorSettings() throws FileNotFoundException, IOException {
+		Resource resource = resourceLoader.getResource("classpath:key.json");
+		Credentials myCredentials = ServiceAccountCredentials.fromStream(resource.getInputStream());
+		ImageAnnotatorSettings imageAnnotatorSettings = ImageAnnotatorSettings.newBuilder().setCredentialsProvider(FixedCredentialsProvider.create(myCredentials)).build();
+		return imageAnnotatorSettings;
 	}
 
 	private List<AnnotateImageResponse> getResponses(ImageAnnotatorClient vision, Feature feat, Image img) {
